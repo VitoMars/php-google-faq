398384bb75e0707cc5cac5c0689444eb03445bbf
Vito <vito95v@gmail.com>
2021-10-11 17:35:27 +0200

    fix testo

--- index.php
+++ index.php
@@ -28,13 +28,10 @@ $faqs = [
        <ol>
          <li>La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:</li>
            <ol>
-
-            <li>Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda,         Liechtenstein e Norvegia) o in Svizzera.</li>
-
+            <li>Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.</li>
             <li>Google LLC, con sede negli Stati Uniti, per il resto del mondo.</li>
-
            </ol>
-            </li>
+         </li>
          <li>La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.</li>
        </ol>
        Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.
